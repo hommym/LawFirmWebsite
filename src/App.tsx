ff3a4c711053395e78f2e241d81fba666d0df04d
FosterSOAsare <fostersoasare@gmail.com>
2024-05-05 17:56:18 +0000

    Pages setup

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layouts/MainLayout";
+import Homepage from "./pages/Homepage";
+import Contact from "./pages/Contact";
+import Philosophy from "./pages/About/Philosophy";
+import Team from "./pages/About/Team";
+import History from "./pages/About/History";
+import Clients from "./pages/Clients";
+
+function App() {
+	return (
+		<main>
+			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<Homepage />}></Route>
+					<Route path="contact" element={<Contact />}></Route>
+
+					<Route path="contact">
+						<Route path="our-philosophy" element={<Philosophy />}></Route>
+						<Route path="history" element={<History />}></Route>
+						<Route path="our-team" element={<Team />}></Route>
+					</Route>
+
+					<Route path="our-clients" element={<Clients />}></Route>
+				</Route>
+			</Routes>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/pages/About/Philosophy.tsx
@@ -0,0 +1,5 @@
+const Philosophy = () => {
+	return <div>Philosophy</div>;
+};
+
+export default Philosophy;
