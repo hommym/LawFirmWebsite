7aee82caa15af08f3b9ff324acc7c9a8c4f959f1
FosterSOAsare <fostersoasare@gmail.com>
2024-05-06 10:01:15 +0000

    Contact page responsiveness

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -5,16 +5,18 @@ import DropDown from "./NavDrops";
 
 import LogoImage from "../../assets/logo.jpeg";
 
+import { AiOutlineMenuFold } from "react-icons/ai";
+
 const Header = ({ isTransparent = false, sx = "" }: { isTransparent?: boolean; sx?: string }) => {
 	const { pathname } = useLocation();
 	return (
-		<header className={`${sx} ${!isTransparent ? "bg-black" : "bg-transparent"} absolute w-full py-2 top-0 left-0`}>
-			<div className="w-full flex items-center max-w-6xl justify-between mx-auto">
-				<Link to="/" className="w-[200px] h-12 ">
+		<header className={`${sx} ${!isTransparent ? "bg-black" : "bg-transparent"} absolute px-4 w-full py-2 top-0 left-0`}>
+			<div className="w-full flex items-center h-auto  max-w-6xl justify-between mx-auto">
+				<Link to="/" className="w-[150px] md:w-[200px] md:h-12 ">
 					<img src={LogoImage} alt="" />
 				</Link>
 
-				<nav className="h-full flex gap-12 items-stretch">
+				<nav className="hidden h-full md:flex gap-12 items-stretch">
 					{links?.map((link, index) => (
 						<DropDown {...link} key={index} isActive={pathname?.startsWith(link.check)} />
 					))}
@@ -25,6 +27,10 @@ const Header = ({ isTransparent = false, sx = "" }: { isTransparent?: boolean; s
 						Contact Us
 					</Link>
 				</nav>
+
+				<button className="md:hidden w-8 h-8 bg-sec flex items-center justify-center">
+					<AiOutlineMenuFold className="text-2xl" />
+				</button>
 			</div>
 		</header>
 	);
--- src/components/molecules/NavDrops.tsx
+++ src/components/molecules/NavDrops.tsx
@@ -23,12 +23,12 @@ const DropDown = ({ name, sub, isActive }: IDropDown) => {
 		<div className="h-auto flex items-center justify-center" ref={parentRef}>
 			<div className={`relative ${showSolutionDropdown ? " md:rounded-full overflow-hidden md:overflow-visible" : ""}`}>
 				<button className="flex group items-center gap-[3px] justify-between">
-					<p className={`${isActive ? "text-sec" : "text-white group-hover:text-sec"}  font-poppins text-sm"`}>{name}</p>
+					<p className={`${isActive ? "text-sec" : "text-white group-hover:text-sec"}  font-poppins text-sm`}>{name}</p>
 					<FiChevronDown className={`${isActive ? "text-sec" : "group-hover:text-sec text-white"}`} />
 				</button>
 				{showSolutionDropdown && (
 					<>
-						<div className="absolute top-[150%] flex flex-col items-start justify-start -left-[10px] w-auto min-w-[150px] h-auto p-3 bg-sec2">
+						<div className="absolute top-[250%] flex flex-col items-start justify-start -left-[10px] w-auto min-w-[150px] h-auto p-3 bg-sec2">
 							{sub?.map((s, index) => (
 								<Link to={s?.link} className="flex text-[13px] w-full justify-start rounded-[5px] hover:bg-black/50 p-2" key={index}>
 									{s?.name}
